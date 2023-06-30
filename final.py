1c02aaf87c4b26b719571e139fd7af4e8d8a64cc
karan <karanishudesai2@gmail.com>
2023-06-30 13:48:35 +0530

    initail commit

--- final.py
+++ final.py
@@ -12,10 +12,49 @@ with open('mapfile1.json') as f:
 restructured_data = []
 
 for entry in data['value']:
-    item = {}
-    for field, mapping in field_mappings_data.items():
-        if mapping is not None:
-            item[field] = entry.get(mapping)
+    item = {
+        'TERABITZ_ID': entry.get(field_mappings_data['TERABITZ_ID']),
+        'ID': entry.get(field_mappings_data['ID']),
+        'PROPERTY': {
+            'ADDRESS': entry.get(field_mappings_data['ADDRESS']),
+            'CITY': entry.get(field_mappings_data['CITY']),
+            'STATE': entry.get(field_mappings_data['STATE']),
+            'ZIP': entry.get(field_mappings_data['ZIP']),
+            'COUNTY': entry.get(field_mappings_data['COUNTY']),
+            'LOCATION': {
+                'latitude': entry.get(field_mappings_data['latitude']),
+                'longitude': entry.get(field_mappings_data['longitude'])
+            }
+        },
+        'MEDIA': {
+            'LISTING_URL': entry.get(field_mappings_data['LISTING_URL']),
+            'VTOUR_URL': entry.get(field_mappings_data['VTOUR_URL']),
+            'MODIF_TIMESTAMP': entry.get(field_mappings_data['MODIF_TIMESTAMP'])
+        },
+        'DATASOURCE': entry.get(field_mappings_data['DATASOURCE']),
+        'UNIT_NUMBER': entry.get(field_mappings_data['UNIT_NUMBER']),
+        'TYPE': entry.get(field_mappings_data['TYPE']),
+        'SUBTYPE': entry.get(field_mappings_data['SUBTYPE']),
+        'TITLE': entry.get(field_mappings_data['TITLE']),
+        'DESCRIPTION': entry.get(field_mappings_data['DESCRIPTION']),
+        'PARCEL_NUMBER': entry.get(field_mappings_data['PARCEL_NUMBER']),
+        'BEDS': entry.get(field_mappings_data['BEDS']),
+        'HALFBATHS': entry.get(field_mappings_data['HALFBATHS']),
+        'LOTSQFT': entry.get(field_mappings_data['LOTSQFT']),
+        'YEAR_BUILT': entry.get(field_mappings_data['YEAR_BUILT']),
+        'DISPLAY_ADDRESS': entry.get(field_mappings_data['DISPLAY_ADDRESS']),
+        'DISPLAY_LISTING': entry.get(field_mappings_data['DISPLAY_LISTING']),
+        'MLS_NUMBER': entry.get(field_mappings_data['MLS_NUMBER']),
+        'STATUS': entry.get(field_mappings_data['STATUS']),
+        'LISTDATE': entry.get(field_mappings_data['LISTDATE']),
+        'ORIG_LISTPRICE': entry.get(field_mappings_data['ORIG_LISTPRICE']),
+        'CURR_LISTPRICE': entry.get(field_mappings_data['CURR_LISTPRICE']),
+        'PHOTO_COUNT': entry.get(field_mappings_data['PHOTO_COUNT']),
+        'PHOTO_MODIF_DATE': entry.get(field_mappings_data['PHOTO_MODIF_DATE']),
+        'ELEM_SCHOOL': entry.get(field_mappings_data['ELEM_SCHOOL']),
+        'MIDL_SCHOOL': entry.get(field_mappings_data['MIDL_SCHOOL']),
+        'HIGH_SCHOOL': entry.get(field_mappings_data['HIGH_SCHOOL'])
+    }
     restructured_data.append(item)
 
 # Print the restructured data
